5e75fdcbd2f8982b32e9da947dede66415960992
Merouane <marouantech380@gmail.com>
2025-11-20 14:57:00 +0100

    commit1

--- src/main/java/com/polytech/App.java
+++ src/main/java/com/polytech/App.java
@@ -1,5 +1,7 @@
 package com.polytech;
 
+import com.polytech.tp.Cours;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Cours GL = new Cours.Builder()
+        .setMatiere("Genie Logiciel")
+        .setEnseignant("cdt Mazari")
+        .setSalle("info6")
+        .setDate("2025-11-20")
+        .setHeureDebut("14:20")
+        .setOptionnel(false)
+        .setNiveau("M1")
+        .setNecessiteProjecteur(true)
+        .build();
     }
 }
